a8742996af28b93098ab8806801cf7818ca577e4
Biegon <156933863+Bigoncode@users.noreply.github.com>
2024-03-26 17:11:23 +0300

    learning c codes

--- size.c
+++ size.c
@@ -3,6 +3,7 @@ int main(){
     int myNumbers[]={20,25,30,40,50};
     printf("%lu\n",sizeof(myNumbers));
     int length;
+    int length;
     length = sizeof(myNumbers)/sizeof(myNumbers[0]);
     printf("%d",length);
      for(int i =0; i<length; i++){
